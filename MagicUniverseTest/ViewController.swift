c9b8d5c7137f8777c1c640fa2d6b1229aa705b16
Evgeniy <evgeniy.freshlife@gmail.com>
2019-05-15 18:01:35 +0300

    Complete label

--- MagicUniverseTest/ViewController.swift
+++ MagicUniverseTest/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
+    
 
-
+    
+    // MARK: Hide Status bar
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+        
 }
+
+// myLabelView constraints
+
 
