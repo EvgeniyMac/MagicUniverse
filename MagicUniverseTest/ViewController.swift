cfd29e4b1d8744b83bbdd66c7b6a23c89eadbc3a
Evgeniy <evgeniy.freshlife@gmail.com>
2019-05-15 16:32:24 +0300

    Initial Commit

---
+++ MagicUniverseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicUniverseTest
+//
+//  Created by Evgeniy Suprun on 15/05/2019.
+//  Copyright © 2019 Evgeniy Suprun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
